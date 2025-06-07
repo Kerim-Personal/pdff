e9e63bf309ca9eb9b9933ba84ff73f33ce1ed245
Kerim-Personal <batinkerimkaraman@gmail.com>
2025-06-07 17:42:21 +0300

    Update

--- app/src/main/java/com/example/pdf/settings_activity.kt
+++ app/src/main/java/com/example/pdf/settings_activity.kt
@@ -3,13 +3,18 @@ package com.example.pdf
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
@@ -31,6 +36,27 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        // *** BİLDİRİM ÇUBUĞUNU GİZLEMEK İÇİN EKLENEN KOD BAŞLANGICI ***
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+        // *** BİLDİRİM ÇUBUĞUNU GİZLEMEK İÇİN EKLENEN KOD BİTİŞİ ***
+
+        // Layout'a eklenen Toolbar'ı bul ve Action Bar olarak ayarla
+        val toolbar: MaterialToolbar = findViewById(R.id.settingsToolbar)
+        setSupportActionBar(toolbar)
+
+        // Bu kod Toolbar üzerinde çalışacak ve geri tuşunu gösterecektir.
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = getString(R.string.settings_title)
 
